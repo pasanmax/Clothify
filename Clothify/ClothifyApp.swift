30e6883c261c49f0f4e7b3d07a7acb3dca50fb55
pasanmax <pasan.anjana98@outlook.com>
2024-03-09 23:05:35 +0530

    Initial Commit

---
+++ Clothify/ClothifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClothifyApp.swift
+//  Clothify
+//
+//  Created by Pasan Anjana Hettiarachchi on 2024-03-09.
+//
+
+import SwiftUI
+
+@main
+struct ClothifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
---
+++ Clothify/Views/HomeView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Clothify
+//
+//  Created by Pasan Anjana Hettiarachchi on 2024-03-09.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    HomeView()
+}
