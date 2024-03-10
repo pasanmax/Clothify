bab5d05ceb00406c2f6908762aea397679c58d81
pasanmax <pasan.anjana98@outlook.com>
2024-03-10 15:09:00 +0530

    Project structure (UIComponents, Fonts, Model, ModelView, Views)

---
+++ Clothify/UICommon/RoundButton.swift
@@ -0,0 +1,18 @@
+//
+//  RoundButton.swift
+//  Clothify
+//
+//  Created by Pasan Anjana Hettiarachchi on 2024-03-10.
+//
+
+import SwiftUI
+
+struct RoundButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RoundButton()
+}
---
+++ Clothify/UICommon/UIExtension.swift
@@ -0,0 +1,18 @@
+//
+//  UIExtension.swift
+//  Clothify
+//
+//  Created by Pasan Anjana Hettiarachchi on 2024-03-10.
+//
+
+import SwiftUI
+
+struct UIExtension: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UIExtension()
+}
