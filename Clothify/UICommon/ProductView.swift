220ca7563740f65e2883c6280fe3c9f03209b0d8
pasanmax <pasan.anjana98@outlook.com>
2024-03-20 01:38:57 +0530

    Product view

---
+++ Clothify/UICommon/ProductView.swift
@@ -0,0 +1,18 @@
+//
+//  ProductView.swift
+//  Clothify
+//
+//  Created by Pasan Anjana Hettiarachchi on 2024-03-19.
+//
+
+import SwiftUI
+
+struct ProductView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductView()
+}
